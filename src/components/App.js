d08a1b1b21a8f61c84d53c29c9812f1ab28bb069
ShemarG <shemar.gordon32@gmail.com>
2021-05-12 11:57:59 -0400

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,15 @@
+import {Route, Switch, Redirect} from 'react-router-dom'
+import Login from './Login'
+
 function App() {
   return (
-    <div className="App"><h1>Welcome to SocieTeam</h1></div>
+    <div className="App">
+      <Switch>
+        <Route path={['/login']}>
+          <Login />
+        </Route>
+      </Switch>
+    </div>
   );
 }
 
---
+++ src/components/Login.js
@@ -0,0 +1,13 @@
+import StateContext from './contexts/StateContext'
+import { useContext } from 'react'
+
+function Login () {
+
+    return (
+        <>
+            <div class="name-jumbotron">Login</div>
+        </>
+    )
+}
+
+export default Login
